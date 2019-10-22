c78adc001534a564afaf3e98b193997fe05abbbf
luke <lukasz.m.pintal@gmail.com>
2019-10-22 02:54:24 +0200

    update user update: dict input

--- db.py
+++ db.py
@@ -13,6 +13,7 @@ class DataBaseManager():
 			self.cur.close()
 		except Exception as e:
 			print(e)
+
 	# Executes one passed command.
 	def db_fetch(self, command):
 		try:
@@ -25,6 +26,44 @@ class DataBaseManager():
 			return result
 		except Exception as e:
 			print(e)
+
+	def create_table(self):
+		columns_types = ', '.join( [ '"{}" {}'.format(tc[0], tc[1]) for tc in self.table])
+		command = 'CREATE TABLE IF NOT EXISTS {} ( {} );'.format(self.table_name, columns_types)
+		self.db_do( command)
+
+	def insert(self, content):
+		# user_content = ['created_at', 'creator_id','post_type', 'post']
+		columns = ', '.join(['"{}"'.format(col) for col in self.column_names[1:]])
+		values  = ', '.join(["'{}'".format(v) for v in content])
+		command = 'INSERT INTO {} ( {}) VALUES ( {});'.format(self.table_name, columns, values)
+		self.db_do(command)
+
+	def get_row(self, row_id):
+		command =  '''SELECT * from {} WHERE id='{}';'''.format( self.table_name, row_id)
+		row = self.db_fetch(command)
+		return row
+	
+	def get_all_rows(self):
+		command =  '''SELECT * from {} Order By created_at DESC;'''.format( self.table_name)
+		rows = self.db_fetch(command)
+		return rows
+
+	def get_rows_where(self, request_dict):
+		where = ', '.join(["{}='{}'".format(k,v) for k,v in request_dict.items()])
+		command =  '''SELECT * from {} WHERE {} Order By created_at DESC;'''.format( self.table_name, where)
+		rows = self.db_fetch(command)
+		return rows
+
+	def update_row(self, updated_content, row_id):
+		# updated_content = {'k':'v'}
+		update_string = ', '.join(["{}='{}'".format(k,v) for k,v in updated_content.items()])
+		command	= '''UPDATE {} SET {} where id={};'''.format(self.table_name, update_string, row_id)
+		self.db_do(command)	
+
+	def delete_row(self, row_id):
+		command = '''DELETE FROM {} WHERE id='{}';'''.format(self.table_name, row_id)
+		self.db_do( command)
 
 class UserTableDB(DataBaseManager):
 
@@ -47,32 +86,82 @@ class UserTableDB(DataBaseManager):
 		self.dbport = None
 
 	def create_user_table(self):
-		columns_types = ', '.join( [ '"{}" {}'.format(tc[0], tc[1]) for tc in self.table])
-		command = 'CREATE TABLE IF NOT EXISTS {} ( {} );'.format(self.table_name, columns_types)
-		self.db_do( command)
-
+		self.create_table()
 	def create_user(self, user_content):
-		# user_content = ['name', 'password','info', 'email','last_login', 'comments'] Values list
-		columns = ', '.join(['"{}"'.format(col) for col in self.column_names[1:]])
-		values  = ', '.join(["'{}'".format(v) for v in user_content])
-		command = 'INSERT INTO {} ( {}) VALUES ( {});'.format(self.table_name, columns, values)
-		self.db_do(command)
-
-	def get_user(self, user_id):
-		command =  '''SELECT * from {} WHERE id='{}';'''.format( self.table_name, user_id)
-		user = self.db_fetch(command)
-		return user
-	
-	def get_users(self):
-		command =  '''SELECT * from {} Order By created_at DESC;'''.format( self.table_name)
-		users = self.db_fetch(command)
-		return users
-
+		self.insert(self, user_content)
 	def update_user(self, updated_content, user_id):
-		update_string = ', '.join(["{}='{}'".format(k,v) for k,v in updated_content.items()])
-		command	= '''UPDATE {} SET {} where id={};'''.format(self.table_name, update_string, user_id)
-		self.db_do(command)	
-
+		self.update_row(updated_content, user_id)
 	def delete_user(self, user_id):
-		command = '''DELETE FROM {} WHERE id='{}';'''.format(self.table_name, user_id)
-		self.db_do( command)
+		self.delete_row(user_id)
+	def get_user(self, user_id):
+		return self.get_row(user_id)
+	def get_users(self):
+		return self.get_all_rows()
+	def get_users_where(self, request_dict):
+		return self.get_rows_where(request_dict)
+
+
+class PostsTable(DataBaseManager):
+
+	table_name = 'posts'
+	table = [
+		('id', 'serial PRIMARY KEY'),
+		('created_at', 'timestamp'),
+		('creator_id', 'text'),
+		('post_type', 'text'),
+		('post', 'text'),]
+	column_names = [c[0] for c in table]
+	
+	def __init__(self, credentials):
+		self.dbname = credentials['dbname']
+		self.dbuser = credentials['dbuser']
+		self.dbpswd = None
+		self.dbport = None
+
+	def create_post_table(self):
+		self.create_table()
+	def create_post(self, post_data):
+		self.insert(self, post_data)
+	def update_post(self, updated_post_data, post_id):
+		self.update_row(updated_content, post_id)
+	def delete_post(self, post_id):
+		self.delete_row(post_id)
+	def get_post(self, post_id):
+		return self.get_row(post_id)
+	def get_posts(self):
+		return self.get_all_rows()
+	def get_posts_where(self, request_dict):
+		return self.get_rows_where(request_dict)
+
+class CommentsTable(DataBaseManager):
+
+	table_name = 'comments'
+	table = [
+		('id', 'serial PRIMARY KEY'),
+		('created_at', 'timestamp'),
+		('creator_id', 'text'),
+		('comment_target_type', 'text'),
+		('comment_target_id', 'text'),
+		('comment', 'text'),]
+	column_names = [c[0] for c in table]
+	
+	def __init__(self, credentials):
+		self.dbname = credentials['dbname']
+		self.dbuser = credentials['dbuser']
+		self.dbpswd = None
+		self.dbport = None
+
+	def create_comment_table(self):
+		self.create_table()
+	def create_comment(self, comment_data):
+		self.insert(self, comment_data)
+	def update_comment(self, updated_comment_data, comment_id):
+		self.update_row(updated_comment_data, comment_id)
+	def delete_comment(self, comment_id):
+		self.delete_row(comment_id)
+	def get_comment(self, comment_id):
+		return self.get_row(comment_id)
+	def get_comments(self):
+		return self.get_all_rows()
+	def get_comments_where(self, request_dict):
+		return self.get_rows_where(request_dict)
